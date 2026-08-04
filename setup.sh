24c53fb25550050a650f33d8621e9eb946362127
Martins <udris.dev@gmail.com>
2026-08-05 01:18:37 +0300

    Terraform Vim syntax provided

--- setup.sh
+++ setup.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+dir=$HOME/.vim/pack/plugins/start/vim-terraform
+
+mkdir -p $dir
+git clone https://github.com/hashivim/vim-terraform.git $dir
 cp $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 cp $HOME/dotfiles/vimrc $HOME/.vimrc

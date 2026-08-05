8c1b5ef648b532542974af04a6faff88f11b6799
martins <mudris100@gmail.com>
2026-08-05 04:10:38 +0300

    updated setup.sh

--- setup.sh
+++ setup.sh
@@ -2,7 +2,23 @@
 
 dir=$HOME/.vim/pack/plugins/start/vim-terraform
 
+# Previous files/directories to clean up from $HOME
+LEGACY_PATHS=(
+  "$HOME/.vimrc"
+  "$HOME/.vim"
+  "$HOME/.tmux.conf"
+)
+
+echo "Cleaning up dotfiles..."
+for path in "${LEGACY_PATHS[@]}"; do
+  if [ -e "$path" ] || [ -L "$path" ]; then
+    rm -rf "$path"
+    echo "  Removed: $path"
+  fi
+done
+
 mkdir -p $dir
 git clone https://github.com/hashivim/vim-terraform.git $dir
 cp $HOME/dotfiles/tmux.conf $HOME/.tmux.conf
 cp $HOME/dotfiles/vimrc $HOME/.vimrc
+cp -r $HOME/dotfiles/vim/colors $HOME/.vim
